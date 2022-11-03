05d9f185afacc9475c95a6f21d3f664fba4ddb04
Viral TechnoComet <115526018+viraltechnocomet@users.noreply.github.com>
2022-11-03 17:26:31 +0530

    model changes

--- core/models.py
+++ core/models.py
@@ -6,11 +6,3 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class addmanager(models.Model):
-    pass
-
-class addagent(models.Model):
-    pass
-
-class addclient(models.Model):
-    pass
