05d1abc328abbbdd2cf16fc5b7a51599097dc59e
Viral TechnoComet <115526018+viraltechnocomet@users.noreply.github.com>
2022-11-04 12:40:16 +0530

    model adition

--- core/models.py
+++ core/models.py
@@ -6,3 +6,12 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Country(models.Model):
+    country = models.CharField(max_length=60, unique=True)
+    city = models.CharField(max_length=60, unique=True)
+    
+class Category(models.Model):
+    c_name = models.CharField(max_length=255, unique=True)
+    
+class Activity(models.Model):
+    activity_name = models.CharField(max_length=160, unique=True)
