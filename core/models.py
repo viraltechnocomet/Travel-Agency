fbbf4e4786baae3fcdb5b67a954566a4c6a82556
Viral TechnoComet <115526018+viraltechnocomet@users.noreply.github.com>
2023-01-18 13:01:53 +0530

    db changes

--- core/models.py
+++ core/models.py
@@ -40,13 +40,13 @@ class Age(models.Model):
         return self.age
     
 class Season(models.Model):
-    season=models.CharField(max_length=250, unique=True)
+    season = models.CharField(max_length=250, unique=True)
     
     def __str__(self):
         return self.season
      
 class Itinerary(models.Model):
-    data_image=models.ImageField(upload_to='media/', null=True, blank=True)
+    data_image = models.ImageField(upload_to='media/', null=True, blank=True)
     destination = models.CharField(max_length=255, unique=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True, null=True)
@@ -54,32 +54,32 @@ class Itinerary(models.Model):
     activity_name = models.ForeignKey(Activity, on_delete=models.CASCADE, blank=True, null=True)
     age = models.ForeignKey(Age, on_delete=models.CASCADE, blank=True, null=True)
     season = models.ForeignKey(Season, on_delete=models.CASCADE, blank=True, null=True)
-    description=models.TextField()
-    befor_you_go=models.TextField()
-    nature=models.CharField(max_length=250)
-    website=models.CharField(max_length = 500)
-    link=models.CharField(max_length=500)
-    gps_cordinate=models.CharField(max_length=500)
-    phone_no=models.CharField(max_length=20, null=True)
+    description = models.TextField()
+    befor_you_go = models.TextField()
+    nature = models.CharField(max_length=250)
+    website = models.CharField(max_length = 500)
+    link = models.CharField(max_length=500)
+    gps_cordinate = models.CharField(max_length=500)
+    phone_no = models.CharField(max_length=20, null=True)
     # spend_time=models.DateTimeField()
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.destination
     
     
 class Package(models.Model):
-    package_image=models.ImageField(upload_to='media/', null=True, blank=True)
-    package_name=models.CharField(max_length=250,unique=True)
-    itinerary_details=models.ManyToManyField(Itinerary, blank=True)
-    from_date=models.DateField(blank=True,null=True)
-    to_date=models.DateField(blank=True,null=True)
-    price=models.CharField(max_length=250, null=True)
-    days=models.CharField(max_length=250, null=True)
-    nights=models.CharField(max_length=250, null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    package_image = models.ImageField(upload_to='media/', null=True, blank=True)
+    package_name = models.CharField(max_length=250,unique=True)
+    itinerary_details = models.ManyToManyField(Itinerary, blank=True)
+    from_date = models.DateField(blank=True,null=True)
+    to_date = models.DateField(blank=True,null=True)
+    price = models.CharField(max_length=250, null=True)
+    days = models.CharField(max_length=250, null=True)
+    nights = models.CharField(max_length=250, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.package_name   
@@ -88,20 +88,20 @@ class Package(models.Model):
 class AddCartPackage(models.Model):
     itinerary_cart = models.ManyToManyField(Itinerary,blank=True)
     # itinerary_cart=models.CharField(max_length=250,blank=True,null=True)
-    start_date=models.DateField(blank=True,null=True)
-    end_date=models.DateField(blank=True,null=True)
-    adults=models.CharField(max_length=250,blank=True,null=True)
-    children=models.CharField(max_length=250,blank=True,null=True)
-    infant=models.CharField(max_length=250,blank=True,null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    start_date = models.DateField(blank=True,null=True)
+    end_date = models.DateField(blank=True,null=True)
+    adults = models.CharField(max_length=250,blank=True,null=True)
+    children = models.CharField(max_length=250,blank=True,null=True)
+    infant = models.CharField(max_length=250,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     
 class Selected_Package(models.Model):
-    use_id=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    package_id=models.ForeignKey(Package,on_delete=models.CASCADE)
-    person=models.IntegerField()
-    costing=models.FloatField(max_length=100)
-    arriaval_time=models.DateTimeField()
+    use_id = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    package_id = models.ForeignKey(Package,on_delete=models.CASCADE)
+    person = models.IntegerField()
+    costing = models.FloatField(max_length=100)
+    arriaval_time = models.DateTimeField()
     
     
